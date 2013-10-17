5df794917a9bd51aec79b57a976bc534d89b798d
wallnerryan <wallnerryan@gmail.com>
2013-10-17 12:09:20 -0400

    add scripts

---
+++ lvm-scripts/lvm-create-vol.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#Logical Volume Create Script v1.0
+
+
+# Istrctions
+# ---------
+# This script take 2 argunments
+# *Numbers of volumes to create & *size of the volumes in GB
+#
+# Use "lvdisplay" to view volumes created
+
+
+#VARS
+
+VOL_GROUP="vg0"  #Defines the volume group to create volumes on
+if [ $1 ];
+then
+    VOLUMES=$1;
+else
+    echo "Number of volumes needed";
+    exit;
+fi
+
+if [ $2 ];
+then
+    SIZE=$2;
+else
+    echo "Size of volumes to be created needed in (GB)";
+    exit;
+fi
+
+# --------------------------------------------------
+# If all params excepted create the number $VOLUMES
+# logical volumes of $SIZE GB on vg $VOL_GROUP
+
+VOL=0
+while [ $VOL -lt $VOLUMES ];
+do
+    #create volumes
+    lvcreate -L $SIZE -n vol$VOL $VOL_GROUP;
+    VOL=$(( $VOL + 1 ));
+done
---
+++ lvm-scripts/lvm-delete-vol.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#Logical Volume Create Script v1.0
+
+
+# Istrctions
+# ---------
+# This script take 2 argunments
+# *Number of volume from vol#  & *(optional) to vol# 
+#
+# Use "lvdisplay" to view volumes created
+
+
+#VARS
+
+VOL_GROUP="vg0"  #Defines the volume group to create volumes on
+if [ $1 ];
+then
+    FROM_VOL=$1;
+else
+    echo "From Volume# needed";
+    exit;
+fi
+
+if [ $2 ];
+then
+    TO_VOL=$2;
+else
+    echo "To Volume# not specified, deleteing single volume";
+    TO_VOL=$1
+fi
+
+# --------------------------------------------------
+# If all params excepted delee the number vols 
+# from $FROM_VOL to $TO_VOL
+
+while [ $FROM_VOL -le $TO_VOL ];
+do
+    #create volumes
+    lvremove -f /dev/$VOL_GROUP/vol$FROM_VOL;
+    FROM_VOL=$(( $FROM_VOL + 1 ));
+done
